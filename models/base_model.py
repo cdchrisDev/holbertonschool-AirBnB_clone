c5f71c37fd934cf4cded34f465eee48a1c4481c3
cdchrisDev <juan.feli.lopez@gmail.com>
2024-03-20 18:13:40 +0000

    done

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,6 @@
 """Class BaseModel"""
 
 from os import getenv
-import models
 import uuid
 from datetime import datetime
 
@@ -15,9 +14,24 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """ Init of the base model """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = self.created_at
+        if kwargs is None or kwargs == "":
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
+        else:
+            for key, value in kwargs.items(): # running dict
+                if key != "__class__":
+                    setattr(self, key, value) #dynamically
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.update_at) is str:
+                self.updated_at = datetime.strptime(kwargs[update_at], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
 
     def __str__(self):
         """Str rep of BaseModel Class"""
